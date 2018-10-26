d16805313fb15e5d0c7aed94b8b254bc1e7d74fe
lukechadwick <llukechadwick@gmail.com>
2018-10-26 15:09:08 +1300

    user input

---
+++ userinput.rb
@@ -0,0 +1,8 @@
+puts 'Hello, Enter in your first name:'
+
+# .chomp takes user input
+first_name = gets.chomp
+
+puts "Hello #{first_name}"
+puts "Your name is #{first_name.length} letters long!"
+puts "Your name is #{first_name.reverse} backwards!"
