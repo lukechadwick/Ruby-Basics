1245a32adb6eeceb478c6864fcf2f1113d966bf5
lukechadwick <llukechadwick@gmail.com>
2018-11-19 13:07:16 +1300

    modiles

---
+++ modules.rb
@@ -0,0 +1,26 @@
+module Destructable
+  def destroy(_anyobject)
+    puts 'I will destroy the object'
+  end
+end
+
+class User
+  include Destructable
+  attr_accessor :name, :email
+
+  def initialize(name, email)
+    @name = name
+    @email = email
+  end
+
+  def run
+    puts "Hey I'm running"
+  end
+
+  def self.identify_yourself
+    puts 'Hey I am a class method'
+  end
+  end
+
+user = User.new('Luke', 'email@example.com')
+user.destroy('myname')
